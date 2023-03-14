7c134c0d923425e46ae960bb24782781b56f919e
Pooja Soundale <pooja.soundale@gmail.com>
2023-03-14 22:36:07 +0000

    Add code to get the endpoint url to send some data to the channel 'MSGraphHackTogether' in teams 'MSFT' (incomplete)

--- SendSummarizedEmailToTeams/SendSummarizedEmailToTeams/Controllers/HomeController.cs
+++ SendSummarizedEmailToTeams/SendSummarizedEmailToTeams/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
 using SendSummarizedEmailToTeams.Models;
 using System;
 using System.Collections.Generic;
@@ -23,8 +25,28 @@ namespace SendSummarizedEmailToTeams.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
+            string teamId = "bf93dcbf-7e6b-4373-a567-ea967265d678";
+            string channelId = "19:b28ae3dbbadf484b9877fc342bce5709@thread.tacv2";
+            var scopes = new[] { "ChannelMessage.Send", "Group.ReadWrite.All" };
+
+            var credential = new DefaultAzureCredential();
+            //var graphClient = new GraphServiceClient(credential, scopes);
+            var graphClient = new GraphServiceClient(credential, new[] { "https://graph.microsoft.com/.default" });
+
+            var requestBody = new ChatMessage
+            {
+                Body = new ItemBody
+                {
+                    Content = "Hello world",
+                },
+            };
+
+            var result = graphClient.Teams[$"{teamId}"]
+                .Channels[$"{channelId}"].Messages;
+                //.PostAsync(requestBody);
+
             return View();
         }
 
