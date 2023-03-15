8287f95d5ebaf558a3feb9d0e2edacb8324aeeaa
lawrenceshort <lawrence.short@systemc.com>
2023-03-15 23:22:31 +0000

    Logging to try and work something out

--- SendSummarizedEmailToTeams/SendSummarizedEmailToTeams/SummarizeMessage/TextAnalyticsClientFactory.cs
+++ SendSummarizedEmailToTeams/SendSummarizedEmailToTeams/SummarizeMessage/TextAnalyticsClientFactory.cs
@@ -9,15 +9,25 @@ namespace SendSummarizedEmailToTeams.SummarizeMessage
 {
     public class TextAnalyticsClientFactory : IFactory<TextAnalyticsClient>
     {
-        private CognitiveServicesOptions _options;
+        private readonly ILogger<TextAnalyticsClientFactory> _logger;
+        private readonly CognitiveServicesOptions _options;
 
-        public TextAnalyticsClientFactory(IOptions<CognitiveServicesOptions> options)
+        public TextAnalyticsClientFactory(IOptions<CognitiveServicesOptions> options,
+            ILogger<TextAnalyticsClientFactory> logger)
         {
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
             _options = options.Value;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public TextAnalyticsClient Build()
         {
+            _logger.LogInformation(_options.Endpoint);
+            _logger.LogInformation(_options.Key);
             if (_options.Endpoint == null)
             {
                 throw new InvalidOperationException("You must provide a value for endpoint.");
